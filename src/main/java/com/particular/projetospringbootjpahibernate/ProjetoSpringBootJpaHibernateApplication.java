d2b27bcd5803e08ec00d8875822bbee404ba52d7
Robson Silva <rms2307@outlook.com>
2020-06-01 17:33:48 -0300

    Criando Projeto

---
+++ src/main/java/com/particular/projetospringbootjpahibernate/ProjetoSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.particular.projetospringbootjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/particular/projetospringbootjpahibernate/ProjetoSpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.particular.projetospringbootjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

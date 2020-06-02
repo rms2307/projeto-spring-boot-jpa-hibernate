bf91e91a490f405996b7e0f57c9b65b01f2d447e
Robson Silva <rms2307@outlook.com>
2020-06-02 16:10:47 -0300

    Service layer

--- src/main/java/com/particular/projetospringbootjpahibernate/repositories/UserRepository.java
+++ src/main/java/com/particular/projetospringbootjpahibernate/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.particular.projetospringbootjpahibernate.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
-	
+/* Não é necessario registar a interface UserRepository como um componente do Spring, 
+pois ela já herda de JpaRepository que já esta registrado como um componente do Spring*/
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }

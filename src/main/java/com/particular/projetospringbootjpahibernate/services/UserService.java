e186b4f6ded6fc19b76aee2ab1ee920b5ff66add
Robson Silva <rms2307@outlook.com>
2020-06-04 19:29:12 -0300

    Exception handling - delete

--- src/main/java/com/particular/projetospringbootjpahibernate/services/UserService.java
+++ src/main/java/com/particular/projetospringbootjpahibernate/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.particular.projetospringbootjpahibernate.entities.User;
 import com.particular.projetospringbootjpahibernate.repositories.UserRepository;
+import com.particular.projetospringbootjpahibernate.services.exceptions.DatabaseException;
 import com.particular.projetospringbootjpahibernate.services.exceptions.ResourceNotFoundException;
 
 @Service // Resgistro da classe como um componente do Spring
@@ -30,7 +33,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);			
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {

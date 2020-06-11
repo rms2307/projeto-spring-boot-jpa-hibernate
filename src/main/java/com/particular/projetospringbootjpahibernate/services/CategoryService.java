6f3fb15f656cfa13aa39f247dec5c5da967e9867
Robson Silva <rms2307@outlook.com>
2020-06-11 18:41:32 -0300

    Category insert

--- src/main/java/com/particular/projetospringbootjpahibernate/services/CategoryService.java
+++ src/main/java/com/particular/projetospringbootjpahibernate/services/CategoryService.java
@@ -23,4 +23,8 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
 }
